83e048578a7e9897727fac56f9471076928927ac
Donovan Jones <donovanljones94@gmail.com>
2019-07-05 17:57:22 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
     puts "You guessed the correct number!"
   elsif guess === "exit"
     puts "Goodbye!"
-  else 
+  elsif guess != number
     puts "Sorry! The computer guessed #{number}."
   end
 end
