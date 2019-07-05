a1002729b50e580d1334ad037c4dcc959980c56b
Donovan Jones <donovanljones94@gmail.com>
2019-07-05 17:59:51 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,14 +1,4 @@
-def run_guessing_game
-  number = rand(6) + 1
-  guess = gets.to_i
-  if guess === number
-    puts "You guessed the correct number!"
-  elsif guess === "exit"
-    puts "Goodbye!"
-  elsif guess != number
-    puts "Sorry! The computer guessed #{number}."
-  end
-end
+
 
 def run_guessing_game
 	puts "/Guess a number betwen 1 and 6."
